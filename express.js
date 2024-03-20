4c23e30910cae1c47cf109e274a1727478aa6533
Sheikhmirfan1 <Sheikhmirfan@yahoo.com>
2024-03-20 15:17:21 +0100

    added

--- express.js
+++ express.js
@@ -1,7 +1,9 @@
 import express, { request } from "express";
-import 'dotenv/config'
+import "dotenv/config";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -9,38 +11,25 @@ app.use(express.urlencoded({ extended: true }));
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "localhost";
 
-
 const cars = [
-  { id: 1, brand: "Toyota", model: "Corolla" },
-  { id: 2, brand: "Toyota", model: "Camry" },
-  { id: 3, brand: "Toyota", model: "Rav4" },
-  { id: 4, brand: "Toyota", model: "Highlander" },
-  { id: 5, brand: "Toyota", model: "4Runner" },
-  { id: 6, brand: "Toyota", model: "Tacoma" },
-  { id: 7, brand: "Toyota", model: "Tundra" },
-  { id: 8, brand: "Toyota", model: "Sienna" },
-  { id: 9, brand: "Toyota", model: "Sequoia" },
-  { id: 10, brand: "Toyota", model: "Land Cruiser" },
-  { id: 11, brand: "Toyota", model: "Supra" },
-  { id: 12, brand: "Toyota", model: "86" },
-  { id: 13, brand: "Toyota", model: "Yaris" },
-  { id: 14, brand: "Toyota", model: "Avalon" },
-  { id: 15, brand: "Toyota", model: "Prius" },
-  { id: 16, brand: "Toyota", model: "Mirai" },
-  { id: 17, brand: "Toyota", model: "C-HR" },
-  { id: 18, brand: "Toyota", model: "RAV4 Prime" },
-  { id: 19, brand: "Toyota", model: "Corolla Hatchback" },
-  { id: 20, brand: "Toyota", model: "Corolla Hybrid" },
-  { id: 21, brand: "Toyota", model: "Camry Hybrid" },
-  { id: 22, brand: "Toyota", model: "Avalon Hybrid" },
-  { id: 23, brand: "Toyota", model: "Sienna Hybrid" },
-  { id: 24, brand: "Toyota", model: "Highlander Hybrid" },
-  { id: 25, brand: "Toyota", model: "4Runner" },
-  { id: 26, brand: "Toyota", model: "Tacoma" },
-  { id: 27, brand: "Toyota", model: "Tundra" },
-  { id: 28, brand: "Toyota", model: "Sequoia" },
-  { id: 29, brand: "Toyota", model: "Land Cruiser" },
-  { id: 30, brand: "Toyota", model: "Supra" },
+  { id: 1, brand: "Toyota", model: "Corolla", year: 2021 },
+  { id: 2, brand: "Toyota", model: "Camry", year: 2022 },
+  { id: 3, brand: "Toyota", model: "Rav4", year: 2022 },
+  { id: 4, brand: "Toyota", model: "Highlander", year: 2022 },
+  { id: 5, brand: "Toyota", model: "4Runner", year: 2023 },
+  { id: 6, brand: "Toyota", model: "Tacoma", year: 2023 },
+  { id: 7, brand: "Toyota", model: "Tundra", year: 2023 },
+  { id: 8, brand: "Toyota", model: "Sienna", year: 2023 },
+  { id: 9, brand: "Toyota", model: "Sequoia", year: 2023 },
+  { id: 10, brand: "Toyota", model: "Land Cruiser", year: 2024 },
+  { id: 11, brand: "Toyota", model: "Supra", year: 2024 },
+  { id: 12, brand: "Toyota", model: "86", year: 2024 },
+  { id: 13, brand: "Toyota", model: "Yaris", year: 2024 },
+  { id: 14, brand: "Toyota", model: "Avalon", year: 2024 },
+  { id: 15, brand: "Toyota", model: "Prius", year: 2024 },
+  { id: 16, brand: "Toyota", model: "Mirai", year: 2020 },
+  { id: 17, brand: "Toyota", model: "C-HR", year: 2024 },
+  { id: 18, brand: "Toyota", model: "RAV4 Prime", year: 2024 },
 ];
 app.get("/", (request, response) => {
   response.send("Hello World");
@@ -48,9 +37,7 @@ app.get("/", (request, response) => {
 
 app.get("/cars", (request, response) => {
   response.json(cars);
-  
 });
-
 
 app.get("/cars/:carsID", (request, response) => {
   const carId = request.params.carsID;
@@ -69,11 +56,8 @@ app.post("/cars", (request, response) => {
 
   cars.push(newCar);
   response.json(newCar);
-}
-);
-
-app.listen(port,host, () => {
-  console.log("Server is running on port 3000");
 });
 
-
+app.listen(port, host, () => {
+  console.log("Server is running on port 3000");
+});
