9a302b50d7522f5aef70cb6e0c5049f0cdc93408
Sheikhmirfan1 <Sheikhmirfan@yahoo.com>
2024-03-25 13:06:37 +0100

    added img

--- index.js
+++ index.js
@@ -13,14 +13,23 @@ const port = process.env.PORT || 3000;
 const host = process.env.HOST || "localhost";
 
 const cars = [
-  { id: 1, brand: "Toyota", model: "Corolla", year: 2021 },
-  { id: 2, brand: "Toyota", model: "Camry", year: 2022 },
-  { id: 3, brand: "Toyota", model: "Rav4", year: 2022 },
-  { id: 4, brand: "Toyota", model: "Highlander", year: 2022 },
-  { id: 5, brand: "Toyota", model: "4Runner", year: 2023 },
-  { id: 6, brand: "Toyota", model: "Tacoma", year: 2023 },
-  { id: 7, brand: "Toyota", model: "Tundra", year: 2023 },
-  { id: 8, brand: "Toyota", model: "Sienna", year: 2023 },
+  { id: 1, brand: "Toyota", model: "Corolla", year: 2021,
+URL: "https://www.toyota.com/corolla/"},
+  { id: 2, brand: "Toyota", model: "Camry", year: 2022, 
+  URL: "https://www.toyota.com/camry/"},
+  { id: 3, brand: "Toyota", model: "Rav4", year: 2022, 
+  URL: "https://www.toyota.com/rav4/"},
+  { id: 4, brand: "Toyota", model: "Highlander", year: 2022,
+  URL: "https://www.toyota.com/highlander/",
+Image: "https://www.toyota.com/highlander/"},
+  { id: 5, brand: "Toyota", model: "4Runner", year: 2023,
+  URL: "https://www.toyota.com/4runner/"},
+  { id: 6, brand: "Toyota", model: "Tacoma", year: 2023,
+  URL: "https://www.toyota.com/tacoma/"},
+  { id: 7, brand: "Toyota", model: "Tundra", year: 2023 ,
+  URL: "https://www.toyota.com/tundra/"},
+  { id: 8, brand: "Toyota", model: "Sienna", year: 2023 ,
+  URL: "https://www.toyota.com/sienna/"},
   { id: 9, brand: "Toyota", model: "Sequoia", year: 2023 },
   { id: 10, brand: "Toyota", model: "Land Cruiser", year: 2024 },
   { id: 11, brand: "Toyota", model: "Supra", year: 2024 },
@@ -56,8 +65,20 @@ app.post("/cars", (request, response) => {
     year: request.body.year,
   };
 
+
+
   cars.push(newCar);
   response.json(newCar);
+});
+
+app.put("/cars/:carsID", (request, response) => {
+  const carId = request.params.carsID;
+  const car = cars.find((car) => car.id === parseInt(carId));
+  car.brand = request.body.brand;
+  car.model = request.body.model;
+  car.year = request.body.year;
+
+  response.json(car);
 });
 
 app.listen(port, host, () => {
